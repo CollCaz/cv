2ee9c05c606170971352bd53e1ac6fd2d1ed8eef
CollCaz <yassentombok@proton.me>
2025-08-03 05:53:30 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: 2024 Akira Komamura
+# SPDX-License-Identifier: Unlicense
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      systems,
+      nixpkgs,
+      treefmt-nix,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = f: lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+    in
+    {
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixpkgs-fmt
+
+            typst
+            tinymist
+            zathura
+          ];
+        };
+      });
+
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+
+      checks = eachSystem (pkgs: {
+        treefmt = treefmtEval.${pkgs.system}.config.build.check self;
+      });
+    };
+}
